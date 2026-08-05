ad132d21836c3310669aa70723530af3f4fd56b4
zeashel <182934048+zeashel@users.noreply.github.com>
2026-08-05 16:52:51 +0700

    Improve catToHumanAge by sanitizing input
    - integers that are too small or too large will return an error string
    - this prevents the result text from overflowing the screen

--- assets/js/catagecalc.js
+++ assets/js/catagecalc.js
@@ -1,7 +1,7 @@
-/* 
- * Copyright (c) 2025. Zahra A. S., All rights reserved. 
- * This design is the intellectual property of Zahra A. S. 
- * Unauthorized use, reproduction, or distribution of this design is prohibited. 
+/*
+ * Copyright (c) 2025. Zahra A. S., All rights reserved.
+ * This design is the intellectual property of Zahra A. S.
+ * Unauthorized use, reproduction, or distribution of this design is prohibited.
  * For permissions, contact 182934048+zeashel@users.noreply.github.com
  */
 
@@ -14,11 +14,20 @@ function liveInputHandler() {
 }
 
 function catToHumanAge(catAge) {
-    pre = '<span style="color:var(--acc-fg);">'
-    post = '</span>'
+    pre = '<span style="color:var(--acc-fg);">';
+    post = '</span>';
+
+    // sanitize
     if (catAge == "") {
         return "unknown";
     }
+    else if (catAge < 0) {
+        return "int too small";
+    }
+    else if (catAge > 999) {
+        return "int too large";
+    }
+
     // 0 <= x < 1
     else if (catAge >= 0 && catAge < 1) {
         return `around ${pre}${Math.round(15 * catAge)}${post}`;
