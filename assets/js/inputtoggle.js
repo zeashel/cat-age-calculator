2a50df1f4f3220b20332135ea90c343a44c1946f
zeashel <182934048+zeashel@users.noreply.github.com>
2026-08-06 21:53:20 +0700

    Implement javascript to toggle input mode from int to dob

---
+++ assets/js/inputtoggle.js
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2026. Zahra A. S., All rights reserved.
+ * This design is the intellectual property of Zahra A. S.
+ * Unauthorized use, reproduction, or distribution of this design is prohibited.
+ * For permissions, contact 182934048+zeashel@users.noreply.github.com
+ */
+
+const checkbox = document.getElementById("inputToggleCheckbox"); // when checked, shows DoB mode and hides int mode
+const intInput = document.getElementById("intInput");
+const dobInput = document.getElementById("dobInput");
+
+checkbox.addEventListener("change", () => {
+    if (checkbox.checked) {
+        intInput.style.display = "none";
+        dobInput.style.display = "block";
+    } else {
+        intInput.style.display = "block";
+        dobInput.style.display = "none";
+    }
+});
