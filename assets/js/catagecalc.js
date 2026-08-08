4cc3e88b8064eb79aa93f5501544f63e30d6eb41
zeashel <182934048+zeashel@users.noreply.github.com>
2026-08-08 20:14:19 +0700

    Improve comments in catagecalc.js

--- assets/js/catagecalc.js
+++ assets/js/catagecalc.js
@@ -26,15 +26,18 @@ function validateCatAge(catAge) {
 // - arg: int (cat years)
 // - return: int (human years)
 function calcCatToHumanAge(catAge) {
-    // from 0 = 0 ... 1 = 15 -> 0 <= x <= 1: f(x) = 15x
+    // from 0 = 0 ... 1 = 15
+    // -> 0 <= x <= 1: f(x) = 15x
     if (catAge >= 0 && catAge <= 1) {
         return Math.round(15 * catAge);
     }
-    // from 1 = 15 ... 2 = 24 -> 1 < x <= 2: f(x) = 6 + 9x
+    // from 1 = 15 ... 2 = 24
+    // -> 1 < x < 2: f(x) = 6 + 9x
     else if (catAge > 1 && catAge <= 2) {
         return Math.round(6 + 9 * catAge);
     }
-    // from 2 = 24 ... x = 24 + 4(x - 2) -> x > 2: f(x) = 16 + 4x
+    // from 2 = 24 ... x = 24 + 4(x - 2)
+    // -> x >= 2: f(x) = 16 + 4x
     else if (catAge > 2) {
         return Math.round(16 + 4 * catAge);
     }
